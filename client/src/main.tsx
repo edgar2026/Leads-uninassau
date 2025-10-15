c1c0aac46dbbca1a10e399e54f6c620345f47e14
[dyad] <edgar.tavares2026@gmail.com>
2025-10-15 09:48:06 -0300

    [dyad] Confirmando a configuração e solicitando um refresh para exibir a tela de login. - wrote 1 file(s)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-console.log("Main script loaded and attempting to render App.");
-
 createRoot(document.getElementById("root")!).render(<App />);
